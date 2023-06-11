c3d0cfe09cfa2696c7c36271773d16f6daec3e7c
Muhammad Aniq Aqil Bin Azrai Fahmi <aniqaqil06@gmail.com>
2023-06-11 10:31:50 +0800

    add new material kit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'my-firstproject';
+
+
+export class AppComponent { 
+  title = 'My First Angular App';
+  counter: number=0;
+  nameList =["ali","siti"];
+  name:string="";
+ 
+
+
+  add(){
+    this.counter++;
+  }
+
+
+  substract(){
+    this.counter--;
+  }
+
+
+  addUser(){
+    this.nameList.push(this.name);
+  }
 }
